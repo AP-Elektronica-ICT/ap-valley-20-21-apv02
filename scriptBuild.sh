efd5bbcd572364478bc6255a0384531aff847d96
jb500 <55529549+jb500@users.noreply.github.com>
2020-10-27 10:21:54 +0100

    Update scriptBuild.sh

--- scriptBuild.sh
+++ scriptBuild.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
-# Build APK Files
-./ap-valley-20-21-apv02 gradlew assembleDebug
-echo "Starting emulator"
-
-# Start emulator in background
-./AppData/Local/Android/Sdk/emulator emulator -avd Pixel_3_XL_API_29
-echo "Emulator started"
-
-# Install apk-package
-./ap-valley-20-21-apv02 adb install app/build/outputs/apk/debug/app-debug.apk
 echo "Success"
