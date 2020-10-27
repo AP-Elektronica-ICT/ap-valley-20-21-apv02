72686903a465d6ba15e510973b3e3153bc066702
Stef Martens <stef.m@hotmail.be>
2020-10-27 16:13:12 +0100

    Profile page aangemaakt

--- app/src/main/java/com/example/loginregister/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/loginregister/ui/profile/ProfileFragment.java
@@ -13,23 +13,78 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.loginregister.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.concurrent.Executor;
 
 public class ProfileFragment extends Fragment {
 
-    private ProfileViewModel profileViewModel;
+    TextView naam, gsm, mail;
+    DatabaseReference reff;
+    FirebaseUser muser;
+    FirebaseAuth mAuth;
+    FirebaseFirestore mStore;
+    String userID;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        profileViewModel =
-                new ViewModelProvider(this).get(ProfileViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        naam= view.findViewById(R.id.username_Textview);
+        gsm= view.findViewById(R.id.phonenumber_Textview);
+        mail=view.findViewById(R.id.userEmail_Textview);
+
+
+        muser = FirebaseAuth.getInstance().getCurrentUser();
+        mAuth = FirebaseAuth.getInstance();
+        mStore = FirebaseFirestore.getInstance();
+        userID = mAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = mStore.collection("usersTest").document(userID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                String _naam=documentSnapshot.getString("uname");
+                String _gsm=documentSnapshot.getString("phone");
+                String _mail=documentSnapshot.getString("email");
+                naam.setText(_naam);
+                gsm.setText(_gsm);
+                mail.setText(_mail);
             }
         });
-        return root;
+
+
+/*        reff= FirebaseDatabase.getInstance().getReference().child("usersTest").child("1");
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String _naam=dataSnapshot.child("naam").getValue().toString();
+                String _gsm=dataSnapshot.child("gsm").getValue().toString();
+                String _mail=dataSnapshot.child("mail").getValue().toString();
+                naam.setText(_naam);
+                gsm.setText(_gsm);
+                mail.setText(_mail);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });*/
+
+        return view;
     }
 }
