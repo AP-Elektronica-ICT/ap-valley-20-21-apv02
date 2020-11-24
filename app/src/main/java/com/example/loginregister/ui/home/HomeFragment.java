699469a569a88a55cb3b4b90db845dc664366ebb
Philip De Rudder <philip_de_rudder@hotmail.com>
2020-11-24 18:56:36 +0100

    added screen / schedular

--- app/src/main/java/com/example/loginregister/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/loginregister/ui/home/HomeFragment.java
@@ -1,35 +1,106 @@
 package com.example.loginregister.ui.home;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.loginregister.R;
 
+import static android.content.Context.JOB_SCHEDULER_SERVICE;
+
 public class HomeFragment extends Fragment {
+    private static final String TAG = "MainActivity";
 
-    private HomeViewModel homeViewModel;
+    private static final String BACK_STACK_ROOT_TAG = "navigation_home";
 
+    Button cancel, start, navigatie;
+    NavController navc;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        setHasOptionsMenu(true);
+
+        start = view.findViewById(R.id.schedule_button);
+        cancel = view.findViewById(R.id.cancel_button);
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.popBackStack(BACK_STACK_ROOT_TAG, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+
+
+
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                scheduleJob(v);
             }
         });
-        return root;
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelJob(v);
+            }
+        });
+        navigatie = view.findViewById(R.id.navigation_button);
+        navigatie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GrowSchedules growschedules = new GrowSchedules();
+
+                // Add the new tab fragment
+                fragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment, growschedules)
+                        .addToBackStack(BACK_STACK_ROOT_TAG)
+                        .commit();
+                navc = Navigation.findNavController(view);
+                navc.navigate(R.id.action_navigation_home_to_growSchedules);
+
+
+            }
+
+
+
+
+        });
+
+
+
+
+        return view;
     }
+    public void scheduleJob(View v) {
+        ComponentName componentName = new ComponentName(getActivity(), ExampleJobService.class);
+        JobInfo info = new JobInfo.Builder(123, componentName)
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                .setPersisted(true)
+                .setPeriodic(15 * 60 * 1000)
+                .build();
+        JobScheduler scheduler = (JobScheduler) getContext().getSystemService(JOB_SCHEDULER_SERVICE);
+        int resultCode = scheduler.schedule(info);
+        if (resultCode == JobScheduler.RESULT_SUCCESS) {
+            Log.d(TAG, "Job scheduled");
+        } else {
+            Log.d(TAG, "Job scheduling failed");
+        }
+    }
+
+    public void cancelJob(View v) {
+        JobScheduler scheduler = (JobScheduler) getContext().getSystemService(JOB_SCHEDULER_SERVICE);
+        scheduler.cancel(123);
+        Log.d(TAG, "Job cancelled");
+    }
+
+
 }
