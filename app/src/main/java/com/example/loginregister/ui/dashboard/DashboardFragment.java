67b04b6722f1289a0721f9d891a25c0a109d9a95
Philip De Rudder <philip_de_rudder@hotmail.com>
2020-12-07 09:58:43 +0100

    get growboxes from database
    verkrijgt growboxes nu van database

--- app/src/main/java/com/example/loginregister/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/loginregister/ui/dashboard/DashboardFragment.java
@@ -5,16 +5,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.SearchView;
-import android.widget.TextView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -22,11 +21,16 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.loginregister.R;
 import com.example.loginregister.ScanActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,31 +42,58 @@ public class DashboardFragment extends Fragment {
     private Button scan;
     private int Count=0;
 
-    ListView lv;
-    List<String> mTitel = new ArrayList<String>();
-    List<String> mBeschrijving = new ArrayList<String>();
-    List<Integer> image = new ArrayList<Integer>();
-    //public String mTitel[] = {"Groeibox 1","Groeibox 2"};
-    //public String mBeschrijving[] = {"Tijm", "Rozemarijn"};
-    //public int image[] = {R.drawable.fotogroeibox, R.drawable.fotogroeibox};
+    ListView listView;
+    List<String> mTitle = new ArrayList<>();
+    List<String> mDescription = new ArrayList<>();
+    List<String> images = new ArrayList<>();
+
+    FirebaseAuth mAuth;
+    FirebaseFirestore mStore;
+    String userID;
+    public DashboardFragment() {
+        // Required empty public constructor
+
+    }
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        mTitel.add("Groeibox1");
-        mTitel.add("Groeibox2");
-
-        mBeschrijving.add("Tijm");
-        mBeschrijving.add("Rozemarijn");
-
-        image.add(R.drawable.fotogroeibox);
-        image.add(R.drawable.fotogroeibox);
-
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        listView = (ListView) view.findViewById(R.id.listViewId);
 
-        lv = (ListView) view.findViewById(R.id.idListView);
+        mAuth = FirebaseAuth.getInstance();
+        mStore = FirebaseFirestore.getInstance();
+        userID = mAuth.getCurrentUser().getUid();
 
-        MijnAdapter adapter = new MijnAdapter(getActivity(),mTitel,mBeschrijving,image);
-        lv.setAdapter(adapter);
+        mStore = FirebaseFirestore.getInstance();
+        mStore.collection("Users").document(userID).collection("0").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    List<String> list = new ArrayList<>();
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        list.add(document.getId().toString());
+                        mTitle.add(document.getString("naam"));
+                        mDescription.add(document.getString("growing"));
+                        images.add(document.getString("url"));
+                    }
+                    MyAdapter adapter = new MyAdapter(getActivity(), mTitle, mDescription, images);
+                    listView.setAdapter(adapter);
+
+                    Log.d("geladen", list.toString());
+                } else {
+                    Log.d("wassup", "wassjp");
+                }
+
+            }
+        });
+
+
+
+
+
+
+
 
         scan = (Button) view.findViewById(R.id.scan);
 
@@ -86,12 +117,14 @@ public class DashboardFragment extends Fragment {
             //Wanneer er van de Activity naar het fragement een bericht gestuurd wordt, wordt er een groeibox toegevoegd
             String strtext=getArguments().getString("message");
             if(strtext!=""){
-                mTitel.add(strtext);
-                mBeschrijving.add("Default beschrijving");
-                image.add(R.drawable.fotogroeibox);
+                mTitle.add(strtext);
+                mDescription.add("Default beschrijving");
+                images.add("https://st.depositphotos.com/1654249/2526/i/600/depositphotos_25269357-stock-photo-3d-man-with-red-question.jpg");
             }
         }
         Count++;
+
+
 
         return view;
     }
@@ -116,13 +149,47 @@ public class DashboardFragment extends Fragment {
         }
     }
 
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        List<String> rTitle;
+        List<String> rDescription;
+        List<String> rImgs;
+
+        MyAdapter(Context c, List<String> title, List<String> description, List<String> imgs) {
+            super(c, R.layout.row_list_grow, R.id.title_main1, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rDescription = description;
+            this.rImgs = imgs;
+
+
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row_list_home, parent, false);
+            ImageView images = row.findViewById(R.id.image);
+            TextView myTitle = row.findViewById(R.id.title_main1);
+            TextView myDescription = row.findViewById(R.id.title_sub1);
+
+            // now set our resources on views
+            Picasso.get().load(rImgs.get(position)).into(images);
+            //  images.setImageResource(rImgs.get(position));
+            myTitle.setText(rTitle.get(position));
+            myDescription.setText(rDescription.get(position));
+            return row;
+        }
+    }
+    /*
     class MijnAdapter extends ArrayAdapter<String>{
         Context context;
         List<String> rTitel;
         List<String> rBeschrijving;
         List<Integer> rImgs;
 
-        //MijnAdapter (Context c, String titel[], String beschrijving[], int imgs[]){
 
         MijnAdapter (Context c, List<String> titel, List<String> beschrijving, List<Integer> imgs){
             super(c, R.layout.row_list_home, R.id.title_main1, titel);
@@ -148,5 +215,5 @@ public class DashboardFragment extends Fragment {
             return rij;
         }
     }
-
+*/
 }
