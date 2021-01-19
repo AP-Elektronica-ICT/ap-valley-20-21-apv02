746a4d02d28492118e7a8307ec68c1844dbb2e48
philip <s102279@ap.be>
2021-01-19 03:29:44 +0200

    kleir

--- app/src/main/java/com/example/loginregister/ScanActivity.java
+++ app/src/main/java/com/example/loginregister/ScanActivity.java
@@ -186,7 +186,7 @@ public class ScanActivity extends AppCompatActivity {
               //    _growing= "iets";
                 _url = value.getString("url");
                 int amount = getAmountGrowboxes();
-                amount2 = amount+=1;
+                amount2 = amount+=2;
                 Log.d("AMOUNTBOXES", "value" + amount);
                 String aantal = String.valueOf(amount);
                 // onderstaande moet van realtime growbox worden gehaald
